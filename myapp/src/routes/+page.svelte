297cfa33e80fd525be74c28d1ce839c439c3589f
VazheninAlex <117972360+VazheninAlex@users.noreply.github.com>
2023-03-15 20:28:00 +0300

    ..

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="js">
     import { onMount } from "svelte";
     import { Button, SvelteUIProvider } from "@svelteuidev/core";
-	let data = [{"name":"pH","value":1,"normalValue":7,"minValue":0,"maxValue":14,"colour":0.42857142857142855},{"name":"sensor2","value":27,"normalValue":50,"minValue":0,"maxValue":100,"colour":0.23},{"name":"sensor3","value":14,"normalValue":50,"minValue":0,"maxValue":100,"colour":0.36}];
+	let data = [{"name":"pH","value":7,"normalValue":7,"minValue":0,"maxValue":14,"colour":0.0},{"name":"sensor2","value":27,"normalValue":50,"minValue":0,"maxValue":100,"colour":0.23},{"name":"sensor3","value":14,"normalValue":50,"minValue":0,"maxValue":100,"colour":0.36}];
     onMount(async function () {
         const response = await fetch("http://127.0.0.1:8000/sensor");
         data = await response.json(); 
@@ -18,27 +18,25 @@
 
 <svelte:head>
 	<title>Sibur</title>
-    <!-- Написать какое-нибудь описание вместо svelte demo app -->
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Показатели окружающей среды города Свободный " />
 </svelte:head>
 
 <SvelteUIProvider>
-    {data.length}
     <h1> 
         ESG повестка ГХК города Свободный
     </h1>
-    <p>
+    <p class="des">
         За последние десять лет ESG-повестка заняла важное место в международной бизнес-среде. На принципы устойчивого развития обратили внимание государственные институты, крупные компании и общество в целом. 
         Компания СИБУР внедрила ESG повестку на своих производствах одной из первых в России.
         В городе Свободный Амурской области строится новое предприятие СИБУРа Амурский газохимический комплекс.
         На нашем сайте каждый может найти данные о состоянии окружающей среды в городе Свободный: уровень Ph воды, концентрацию тяжелых металлов в почве и воздухе и тд.
     </p>
     {#each data as number}
-    <p style="margin-top: 100px">
+    <h3 class="name">
         {data ? number.name : null}
-    </p>
-    <div style="width: 100px; height: 48px; margin-left: 400px; margin-top: -50px;">
-        <Button override={{ bc: rgbToHex(255*2*number.colour, 255*(1-2*number.colour), 0), color: '#00ff00', '&:hover': { bc: rgbToHex(255*2*number.colour, 255*(1-2*number.colour), 0) }}} variant='outline'>
+    </h3>
+    <div style="width: 100px; height: 50px; margin-left: 400px; margin-top: -50px;">
+        <Button override={{ bc: rgbToHex(Math.round(255*2*number.colour) , Math.round(255*(1-2*number.colour)), 0), color: '#000000', '&:hover': { bc: rgbToHex(Math.round(255*2*number.colour), Math.round(255*(1-2*number.colour)), 0) }}} variant='outline'>
             {data ? number.value : null}
         </Button>
     </div>
@@ -53,5 +51,17 @@
     }
     .num{
         text-align: center;
+    }
+    .des{
+        margin-left: 2.5%;
+        font-size: 18px;
+    }
+    .name{
+        margin-top: 7.5%;
+        margin-left: 2.5%;
+    }
+    .Button{
+        min-height:1000px; 
+        min-width: 2000px;
     }
 </style>
