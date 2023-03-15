c3ef3fb8ce3b037bc4b93b5e65f6939c36be4253
Al1en <39908226+Al-1-en@users.noreply.github.com>
2023-03-15 09:45:44 +0300

    change SMUI to svetleUI

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="js">
-    const { palette } = createTheme();
-    const { augmentColor } = palette;
-
     import { onMount } from "svelte";
-    import Button from '@smui/button';
+    import { Button, SvelteUIProvider } from "@svelteuidev/core";
 	let data = [{"name":"pH","value":1,"normalValue":7,"minValue":0,"maxValue":14,"colour":0.42857142857142855},{"name":"sensor2","value":27,"normalValue":50,"minValue":0,"maxValue":100,"colour":0.23},{"name":"sensor3","value":14,"normalValue":50,"minValue":0,"maxValue":100,"colour":0.36}];
     onMount(async function () {
         const response = await fetch("http://127.0.0.1:8000/sensor");
@@ -25,27 +22,27 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-{data.length}
-<h1> 
-    ESG повестка НПЗ города Свободный
-</h1>
-<p>
-    Текст
-</p>
-{#each data as number}
-<p style="margin-top: 100px">
-    {data ? number.name : null}
-</p>
-<div  style="width: 100px; height: 48px; border:1px solid; border-radius:10px; margin-left: 400px;
-margin-top: -50px; background: #00ff00;"> 
-<Button>
-    {data ? number.value : null}
-</Button>
-</div>
-	{/each}
-
-<Button color={augmentColor({ color: { main: '#F40B27'} })} variant="raised">123</Button>
-
+<SvelteUIProvider>
+    {data.length}
+    <h1> 
+        ESG повестка НПЗ города Свободный
+    </h1>
+    <p>
+        Текст
+    </p>
+    {#each data as number}
+    <p style="margin-top: 100px">
+        {data ? number.name : null}
+    </p>
+    <!-- <div style="width: 100px; height: 48px; border:1px solid; border-radius:10px; margin-left: 400px; margin-top: -50px; background: #00ff00;"> -->
+    <div style="width: 100px; height: 48px; margin-left: 400px; margin-top: -50px;">
+        <Button override={{ bc: '#ff00ff', color: '#00ff00' }} variant='outline'>
+            {data ? number.value : null}
+        </Button>
+    </div>
+        {/each}
+    <!-- <Button>123</Button> -->
+</SvelteUIProvider>
 
 
 
